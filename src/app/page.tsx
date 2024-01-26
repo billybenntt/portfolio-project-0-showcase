79f1c534c911714ad0498f46419daf32592793f2
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2024-01-26 11:31:17 +0800

    reworked timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,68 +39,43 @@ function MainPage() {
 
             {/*TIMELINE 1 */}
 
-            <section className="timeline-1  show-box ">
-                <div className="timeline-center show-box">
-                    <div className="card">
-                        <div className="card__body">
-                            <h3 className="text-lg">First Year</h3>
-                            <span className="text-md">Some work experience</span>
-                            <p className="text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                                eiusmod tempor
-                                in voluptate.</p>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="card__body">
-                            <h3 className="text-lg">Second Year</h3>
-                            <span className="text-md">Some work experience</span>
-                            <p className="text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                                eiusmod tempor
-                                in voluptate.</p>
+            <section className="timeline-1">
+                <div className="timeline-center">
+                    <div className="timeline-card">
+                        <div className="date">2002</div>
+                        <div className="title">Title 1</div>
+                        <div className="descr">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic
+                            quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!
                         </div>
                     </div>
 
-                    <div className="card">
-                        <div className="card__body">
-                            <h3 className="text-lg">Third Year</h3>
-                            <span className="text-md">Some work experience</span>
-                            <p className="text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                                eiusmod tempor
-                                in voluptate.</p>
+                    <div className="timeline-card">
+                        <div className="date">2007</div>
+                        <div className="title">Title 2</div>
+                        <div className="descr">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos adipisci
+                            nobis
+                            nostrum vero nihil veniam.
                         </div>
                     </div>
-                </div>
-            </section>
-
-            {/*TIMELINE 2*/}
-            <section className="timeline-2  show-box ">
-                <div className="timeline-center show-box">
-                    <div className="card">
-                        <div className="card__body">
-                            <h3 className="text-lg">First Year</h3>
-                            <span className="text-md">Some work experience</span>
-                            <p className="text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                                eiusmod tempor
-                                in voluptate.</p>
+                    <div className="timeline-card">
+                        <div className="date">2012</div>
+                        <div className="title">Title 3</div>
+                        <div className="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga minima
+                            consequuntur soluta placeat iure totam commodi repellendus ea delectus, libero fugit quod
+                            reprehenderit, sequi quo, et dolorum saepe nulla hic.
                         </div>
                     </div>
-                    <div className="card">
-                        <div className="card__body">
-                            <h3 className="text-lg">Second Year</h3>
-                            <span className="text-md">Some work experience</span>
-                            <p className="text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                                eiusmod tempor
-                                in voluptate.</p>
+                    <div className="timeline-card">
+                        <div className="date">2017</div>
+                        <div className="title">Title 4</div>
+                        <div className="descr">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit,
+                            cumque.
                         </div>
                     </div>
-
-                    <div className="card">
-                        <div className="card__body">
-                            <h3 className="text-lg">Third Year</h3>
-                            <span className="text-md">Some work experience</span>
-                            <p className="text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                                eiusmod tempor
-                                in voluptate.</p>
+                    <div className="timeline-card">
+                        <div className="date">2022</div>
+                        <div className="title">Title 5</div>
+                        <div className="descr">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, non.
                         </div>
                     </div>
                 </div>
