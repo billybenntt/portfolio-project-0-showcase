209b1e68b0be12262cc9b7be715d23a3a0c8dede
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2024-02-16 15:31:50 +0800

    styled faq accordion

--- src/components/AccordionComponent.tsx
+++ src/components/AccordionComponent.tsx
@@ -1,42 +1,40 @@
 import * as Accordion from '@radix-ui/react-accordion';
-import {PlusIcon} from 'lucide-react'
+import {PlusIcon, MinusIcon} from 'lucide-react'
 
 function AccordionComponent() {
     return (
         <Accordion.Root type="single" defaultValue="item-1" collapsible>
-
             <div className=" accordion-center">
                 <Accordion.Item value="item-1">
                     <Accordion.Header>
                         <Accordion.Trigger className="question">
                             <span>Lorem ipsum dolor sit amet, elit. Consequuntur, tempore?</span>
-                            <PlusIcon/>
+                            <PlusIcon className="icon-open"/>
+                            <MinusIcon className="icon-close"/>
                         </Accordion.Trigger>
                     </Accordion.Header>
                     <Accordion.Content className="answer">
                         Lorem ipsum dolor sit amet.
                     </Accordion.Content>
                 </Accordion.Item>
-
                 <Accordion.Item value="item-2">
                     <Accordion.Header>
                         <Accordion.Trigger className="question">
                             <span>Lorem ipsum dolor sit amet, elit. Consequuntur, tempore?</span>
-                            <PlusIcon/>
-
+                            <PlusIcon className="icon-open"/>
+                            <MinusIcon className="icon-close"/>
                         </Accordion.Trigger>
                     </Accordion.Header>
                     <Accordion.Content className="answer">
                         Lorem ipsum dolor sit amet.
                     </Accordion.Content>
                 </Accordion.Item>
-
-
                 <Accordion.Item value="item-3">
                     <Accordion.Header>
                         <Accordion.Trigger className="question">
                             <span>Lorem ipsum dolor sit amet, elit. Consequuntur, tempore?</span>
-                            <PlusIcon/>
+                            <PlusIcon className="icon-open"/>
+                            <MinusIcon className="icon-close"/>
                         </Accordion.Trigger>
                     </Accordion.Header>
                     <Accordion.Content className="answer">
