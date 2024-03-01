57792b839a83851da2cf82af30777e24bc4f77ca
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2024-03-01 09:52:33 +0800

    chore: updated portfolio links

--- src/utils/projects.tsx
+++ src/utils/projects.tsx
@@ -5,48 +5,48 @@ export const projects =
             title: 'Project 01',
             subTitle: 'Age Calculator App',
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing",
-            linkSite: "https://www.google.com",
-            linkRepo: "https://www.github.com"
+            linkSite: "https://project01.houseofthe.app/",
+            linkRepo: "https://github.com/billybenntt/portfolio-project-1-age-calculator"
         },
         {
             id: "project02",
             title: 'Project 02',
             subTitle: 'Bill Splitter App',
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing",
-            linkSite: "https://www.google.com",
-            linkRepo: "https://www.github.com"
+            linkSite: "https://project02.houseofthe.app/",
+            linkRepo: "https://github.com/billybenntt/portfolio-project-2-tip_calculator_app"
         },
         {
             id: "project03",
             title: 'Project 03',
             subTitle: 'Github User Search App',
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing",
-            linkSite: "https://www.google.com",
-            linkRepo: "https://www.github.com"
+            linkSite: "https://project03.houseofthe.app/",
+            linkRepo: "https://github.com/billybenntt/portfolio-project-3-github-user-search"
         },
         {
             id: "project04",
             title: 'Project 04',
             subTitle: 'Weather Dashboard',
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing",
-            linkSite: "https://www.google.com",
-            linkRepo: "https://www.github.com"
+            linkSite: "https://project04.houseofthe.app/",
+            linkRepo: "https://github.com/billybenntt/portfolio-project-4-weather-app"
         },
         {
             id: "project05",
             title: 'Project 05',
             subTitle: 'IP Tracker Search App',
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing",
-            linkSite: "https://www.google.com",
-            linkRepo: "https://www.github.com"
+            linkSite: "https://project05b.vercel.app/",
+            linkRepo: "https://github.com/billybenntt/portfolio-project-5-ip_address_tracker"
         },
         {
             id: "project06",
             title: 'Project 06',
             subTitle: 'Invoice Dashboard',
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing",
-            linkSite: "https://www.google.com",
-            linkRepo: "https://www.github.com"
+            linkSite: "https://project05.houseofthe.app/",
+            linkRepo: "https://github.com/billybenntt/portfolio-project-6-invoice_app"
         }
 
     ]
